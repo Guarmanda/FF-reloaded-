9610469a12d2366cf02486aaa49f10f4535e7652
Nathalie Karman <40271378+nathalie-karman@users.noreply.github.com>
2019-04-09 18:00:49 +0200

    Update test_perso.c

--- test_perso.c
+++ test_perso.c
@@ -4,21 +4,22 @@
 
 
 int main(){
-
-    character_t* perso = creation_char();
     init_tab_sort();
+    affich_tab_sort();
+    character_t* perso = creation_char();
+    printf("les stats du perso\n" );
+    affich_stats(perso);
     printf("entrez les coordonnees souhaitées pour x: ");
     scanf("%d", &position_x);
     printf("entrez les coordonnees souhaitées pour y: ");
     scanf("%d", &position_y);
-    character_t* monstre= monster_creation();   /*meme niveau que le joueur*/
-    character_t* monstre2= monster_creation();   /*meme niveau que le joueur*/
-    printf("vous jouez contre :\n");
-    affich_stats(monstre);
-    affich_stats(monstre2);
-    delete_player(&perso);
+
+    character_t* monstre2= monster_creation();
+    printf("sorts du monstre\n" );
+    afficher_sorts(monstre2);
     delete_player(&monstre2);
-    delete_player(&monstre);
+    delete_player(&perso);
+    suppr_tab_sort();
 
     return 0;
 }
