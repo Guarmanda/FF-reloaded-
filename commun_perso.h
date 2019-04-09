959dfa12bb9d17cdc2042e46bbf6310e4959e4d4
Nathalie Karman <40271378+nathalie-karman@users.noreply.github.com>
2019-04-09 13:19:17 +0200

    same

--- commun_perso.h
+++ commun_perso.h
@@ -7,7 +7,7 @@ enum type{ offensif=0, defensif, modifie_etat};
 
 typedef struct spell_s{
    char* nom_sort;
-   enum type type_sort;  /* 0 = offensif/ 1 = defensif/ 2 = state modifier*/
+   enum type type_sort;  /*0 = offensif/ 1 = defensif/ 2 = state modifier*/
    int valeur_sort;
 }spell_t;
 
@@ -30,13 +30,13 @@ typedef struct character_s{
   int max_health;       /*vie maximum que peut avoir le joueur selon son niveau*/
   int max_mana;
   liste_sort_t* liste_spell;           /* structure chainée qui pointera sur le tableau global de sort*/
-  int state[MAX_ETATS];         /*voir possibilité de pouvoir etre sous diff states avec un tableau: blind,stunt,slow,poison,berserk,silence,bleeding*/
+  int state[MAX_ETATS];
   int stat_strength;
   int stat_intelligence;
   int stat_stamina;
-  char * class_char;
-  object_t char_armor;  /*type = 0*/
-  object_t char_weapon;  /*type = 1*/
+  char* class_char;
+  object_t* char_armor;
+  object_t* char_weapon;
   enum accessories_e accessory;
   char* gender;
 }character_t;
@@ -46,14 +46,15 @@ character_t* creation_char();
 
 void init_tab_sort(void);
 void affich_tab_sort(void);
-err_t suppr_tab_sort(spell_t*);
+void afficher_sorts(character_t*);
+err_t suppr_tab_sort();
 character_t* creation_char();
-void delete_player(character_t**);
+void delete_player(character_t** player);
+void supprimer_sorts(character_t* perso);
 void affich_stats(character_t*); /*fonction d'affichage des details du joueur*/
 /*--*/
 character_t* monster_creation();
-int name_and_details(character_t* );
-void init_stats_monstre(character_t* );
+
 /*--*/
 int taking_potion(character_t **player);
 void apply_state_modifier(character_t **, int , int );
